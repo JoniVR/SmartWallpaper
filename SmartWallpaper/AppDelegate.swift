9ca13e9e46359ab24ebff527b2807a316d956bd2
JoniVR <joni.vanroost@student.kdg.be>
2017-11-15 23:04:21 +0100

    Initial Commit

---
+++ SmartWallpaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmartWallpaper
+//
+//  Created by Joni Van Roost on 15/11/17.
+//  Copyright © 2017 Joni Van Roost. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SmartWallpaper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmartWallpaper
+//
+//  Created by Joni Van Roost on 15/11/17.
+//  Copyright © 2017 Joni Van Roost. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
